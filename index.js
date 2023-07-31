da496260fd6fa29f75ba26203696a1e8717712d2
nmke-de <github@nmke.de>
2023-07-31 17:37:32 +0200

    Added markdown and embedded video support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import { Geddit } from "https://github.com/kaangiray26/geddit-app/raw/main/src/js/geddit.js";
 import { Server } from "https://deno.land/std@0.196.0/http/server.ts";
+import { Marked } from "https://raw.githubusercontent.com/ubersl0th/markdown/master/mod.ts";
 
 const g = new Geddit();
 const port = 11100;
@@ -15,6 +16,8 @@ const handler = async (request) => {
 	let res = "";
 	for (let i = 0; i < p.length; i++) {
 		const entry = p[i];
+		// console.log(`${entry.data.post_hint}\t${entry.data.title}`)
+		// console.log(entry.data.post_hint === undefined ? entry.data : "");
 		res += `\t\t<article style='background-color: #f0f0f0;'>
 			<h1>${entry.data.title}</h1>
 			<author>${entry.data.author}</author>
@@ -22,7 +25,8 @@ const handler = async (request) => {
 			<a href="${entry.data.permalink}">Permalink</a>
 			${entry.data.post_hint === "image" ? '<img src="' + entry.data.url_overridden_by_dest + '" alt="Reddit Post" />' : ''}
 			${entry.data.post_hint === "hosted:video" ? '<video controls src="' + entry.data.media.reddit_video.fallback_url + '" alt="Reddit Post" />' : ''}
-			<p>${entry.data.selftext.replace("\n", "</p><p>")}</p>
+			${entry.data.post_hint === "rich:video" ? entry.data.media.oembed.html.replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&amp;/g, "&") : ''}
+			<div>${Marked.parse(entry.data.selftext).content}</div>
 		</article>\n`;
 	}
 	let body = `<!doctype html>
