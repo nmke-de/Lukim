f80c56a09c7469db6b08e583f0e1097004db3b84
nmke-de <github@nmke.de>
2023-07-31 15:50:00 +0200

    Added browsing subreddits and showing images

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ import { Server } from "https://deno.land/std@0.196.0/http/server.ts";
 const g = new Geddit();
 const port = 11100;
 const handler = async (request) => {
-	const p = (await g.getSubmissions()).posts;
+	const query = request.url.slice(("http://" + request.headers.get("host") + "/").length);
+	console.log(query);
+	let subreddit = "";
+	if (query.slice(0, 2) == "r/")
+		subreddit = query.slice(2);
+	const p = (await g.getSubmissions("hot", subreddit)).posts;
+	//console.log(p);
 	let res = "";
 	for (let i = 0; i < p.length; i++) {
 		const entry = p[i];
@@ -15,6 +21,7 @@ const handler = async (request) => {
 			<author>${entry.data.author}</author>
 			<a href="${entry.data.subreddit_name_prefixed}">${entry.data.subreddit_name_prefixed}</a>
 			<a href="${entry.data.permalink}">Permalink</a>
+			${entry.data.post_hint === "image" ? '<img src="' + entry.data.url_overridden_by_dest + '" alt="Reddit Post" />' : ''}
 			<p>${entry.data.selftext.replace("\n", "</p><p>")}</p>
 		</article>\n`;
 	}
